ee1228e1a0d143edd435762983c169114dbd34a8
FredNeves95 <fredbneves95@gmail.com>
2024-03-12 21:25:36 -0300

    feat: set active and default category as other

--- src/main/java/com/courses/courses/modules/courses/useCases/CreateCourseUseCase.java
+++ src/main/java/com/courses/courses/modules/courses/useCases/CreateCourseUseCase.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.courses.courses.modules.courses.dtos.CreateCourseResponseDTO;
 import com.courses.courses.modules.courses.entities.CourseEntity;
+import com.courses.courses.modules.courses.enums.CategoriesEnum;
 import com.courses.courses.modules.courses.repositories.CourseRepository;
 
 @Service
@@ -13,12 +14,19 @@ public class CreateCourseUseCase {
   private CourseRepository courseRepository;
 
   public CreateCourseResponseDTO execute (CourseEntity courseEntity) {
+    if(courseEntity.getActive() == null){
+      courseEntity.setActive(true);
+    }
+    if(courseEntity.getCategory() == null){
+      courseEntity.setCategory(CategoriesEnum.OTHER);
+    }
     var response = courseRepository.save(courseEntity);
     var responseDTO = CreateCourseResponseDTO.builder()
       .id(response.getId())
       .name(response.getName())
       .category(response.getCategory())
       .active(response.getActive())
+      .createdAt(response.getCreatedAt())
       .build();
       return responseDTO;
   }
